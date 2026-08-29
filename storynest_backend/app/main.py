1ffaa2a7f6c3ac0165f42779a8168113b1bec1ec
dhileep8795 <dhileep8795@gmail.com>
2026-08-29 19:13:22 +0530

    allow all origins

--- storynest_backend/app/main.py
+++ storynest_backend/app/main.py
@@ -16,11 +16,7 @@ app = FastAPI(title="InkNest API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://storynest.community1.workers.dev"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
