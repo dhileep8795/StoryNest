c0dc39405e51b6cac251fc5e91ac9f81c833db68
dhileep8795 <dhileep8795@gmail.com>
2026-08-29 19:05:48 +0530

    cors updated

--- storynest_backend/app/main.py
+++ storynest_backend/app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://storynest.dmunige.workers.dev"
+        "https://storynest.community1.workers.dev/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
